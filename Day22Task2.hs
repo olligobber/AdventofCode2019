d558cd3e1cb7025d59a8c904f666b14c05ecc46a
olligobber <sirolligobbervii@yahoo.com.au>
2019-12-22 23:35:54 +1100

    Made syntax nicer

--- Day22Task2.hs
+++ Day22Task2.hs
@@ -1,11 +1,10 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds, TypeOperators #-}
 
-import Data.Modular (Mod, inv)
-import Data.Function (on)
+import Data.Modular (ℤ, type (/)(), inv)
 import Data.Semigroup (stimes)
 
-type Card = Mod Integer 119315717514047
--- type Card = Mod Integer 10007
+type Card = ℤ / 119315717514047
+-- type Card = ℤ / 10007
 
 t :: Integer
 t = 101741582076661
@@ -23,11 +22,7 @@ instance Monoid Linear where
 app :: Linear -> Card -> Card
 app (Linear a b) x = a*x+b
 
-times :: Linear -> Integer -> Linear
-times (Linear a b) n = Linear (a^n) ((a^n - 1) * inv (a-1) * b)
-
 invert :: Linear -> Linear
-invert (Linear 0 b) = error "Divide by zero"
 invert (Linear a b) = Linear (inv a) (-b * inv a)
 
 readop :: String -> Linear
@@ -40,5 +35,5 @@ readshuffle :: String -> Linear
 readshuffle = foldMap readop . lines
 
 main :: IO ()
-main = interact $ (++"\n") . show . flip app 2020 . invert . flip times t . readshuffle
+main = interact $ (++"\n") . show . flip app 2020 . invert . stimes t . readshuffle
 -- main = interact $ (++"\n") . show . flip app 2019 . readshuffle
